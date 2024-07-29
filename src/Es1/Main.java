a9fab826838bed4740f07e8fa8df1db2233434ff
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-29 15:08:00 +0200

    aggiunto controllo scelta 0

--- src/Es1/Main.java
+++ src/Es1/Main.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -26,39 +27,43 @@ public class Main {
             System.out.println("Scegli una posizione da 1 a 5");
             String posizioneScelta = scanner.nextLine();
             int numeroScelto;
-
-            switch (posizioneScelta) {
-                case "1":
-                    System.out.println("Che numero vuoi inserire?");
-                    numeroScelto = Integer.parseInt(scanner.nextLine());
-                    numRandom[0] = numeroScelto;
-                    break;
-                case "2":
-                    System.out.println("Che numero vuoi inserire?");
-                    numeroScelto = Integer.parseInt(scanner.nextLine());
-                    numRandom[1] = numeroScelto;
-                    break;
-                case "3":
-                    System.out.println("Che numero vuoi inserire?");
-                    numeroScelto = Integer.parseInt(scanner.nextLine());
-                    numRandom[2] = numeroScelto;
-                    break;
-                case "4":
-                    System.out.println("Che numero vuoi inserire?");
-                    numeroScelto = Integer.parseInt(scanner.nextLine());
-                    numRandom[3] = numeroScelto;
-                    break;
-                case "5":
-                    System.out.println("Che numero vuoi inserire?");
-                    numeroScelto = Integer.parseInt(scanner.nextLine());
-                    numRandom[4] = numeroScelto;
-                    break;
-                default:
-                    System.out.println("Arrivederci!");
-                    break;
+            if (Objects.equals(posizioneScelta, "0")) {
+                break;
+            } else {
+                
+                switch (posizioneScelta) {
+                    case "1":
+                        System.out.println("Che numero vuoi inserire?");
+                        numeroScelto = Integer.parseInt(scanner.nextLine());
+                        numRandom[0] = numeroScelto;
+                        break;
+                    case "2":
+                        System.out.println("Che numero vuoi inserire?");
+                        numeroScelto = Integer.parseInt(scanner.nextLine());
+                        numRandom[1] = numeroScelto;
+                        break;
+                    case "3":
+                        System.out.println("Che numero vuoi inserire?");
+                        numeroScelto = Integer.parseInt(scanner.nextLine());
+                        numRandom[2] = numeroScelto;
+                        break;
+                    case "4":
+                        System.out.println("Che numero vuoi inserire?");
+                        numeroScelto = Integer.parseInt(scanner.nextLine());
+                        numRandom[3] = numeroScelto;
+                        break;
+                    case "5":
+                        System.out.println("Che numero vuoi inserire?");
+                        numeroScelto = Integer.parseInt(scanner.nextLine());
+                        numRandom[4] = numeroScelto;
+                        break;
+                    default:
+                        System.out.println("Scelta non valida scegli un numero compreso da 1 a 5");
+                        break;
+                }
+                System.out.println(Arrays.toString(numRandom));
+                logger.info(Arrays.toString(numRandom));
             }
-            System.out.println(Arrays.toString(numRandom));
-            logger.info(Arrays.toString(numRandom));
         }
 
 
